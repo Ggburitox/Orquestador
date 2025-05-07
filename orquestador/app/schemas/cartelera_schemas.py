b300597b0d21fa1a2e68edabb9b1da545a1a472b
YiT <yitzhaknamihas@gmail.com>
2025-05-07 15:32:57 -0500

    Delete orquestador directory

--- orquestador/app/schemas/cartelera_schemas.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-class PeliculaResponse(BaseModel):
-    id: int
-    titulo: str
-    descripcion: str
-    duracion: int
--- orquestador/app/services/cartelera_service.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-CARTELERA_URL = "http://cartelera:8080"
-
-def obtener_cartelera():
-    return requests.get(f"{CARTELERA_URL}/peliculas").json()
