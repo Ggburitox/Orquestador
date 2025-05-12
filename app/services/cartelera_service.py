8ec2166f34136f85cc35742f0daec8b65a8aabfe
YiT <yitzhaknamihas@gmail.com>
2025-05-11 20:11:24 -0500

    Update cartelera_service.py

--- app/services/cartelera_service.py
+++ app/services/cartelera_service.py
@@ -5,13 +5,13 @@ CARTELERA_URL = "http://54.166.101.85:8002/peliculas"
 
 async def crear_pelicula(pelicula: PeliculaCreate):
     async with httpx.AsyncClient() as client:
-        response = await client.post(CARTELERA_URL, json=pelicula.dict())
+        response = await client.post(f"{CARTELERA_URL}/", json=pelicula.dict())
         response.raise_for_status()
         return response.json()
 
 async def listar_peliculas():
     async with httpx.AsyncClient() as client:
-        response = await client.get(CARTELERA_URL)
+        response = await client.get(f"{CARTELERA_URL}/")
         response.raise_for_status()
         return response.json()
 
